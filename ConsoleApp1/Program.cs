ea55c17b0c7a0b07cdc0eb47795eb0c6871eec00
EMERSON LUCAS BERTELLI CARDOSO <emerson.cardoso@alunos.sc.senac.br>
2022-10-28 20:42:00 -0300

    teste

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using ConsoleApp1;
+
+Validator validator = new Validator();
+
+string frase = "Halloween is here";
+
+Console.WriteLine($"Frase do desafio: {frase}");
+validator.ValidaMaiuscula();
+validator.ValidaMinuscula();
+validator.ValidaConsoante();
+validator.ValidaVogal();
+validator.ValidaQtdCaracteres();
+validator.ValidaQtdPalavra();
---
+++ ConsoleApp1/Validator.cs
@@ -0,0 +1,117 @@
+﻿namespace ConsoleApp1
+{
+    public class Validator
+    {
+        public string Frase = "Halloween is here";
+        public void ValidaMaiuscula()
+        {
+
+            string maiusculo = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string minusculo = "abcdefghijklmnopqrstuvwxyz";
+
+            int qtdMaiusculo = 0;
+
+
+            for (int i = 0; i < Frase.Length; i++)
+            {
+                for (int j = 0; j < maiusculo.Length; j++)
+                {
+                    if (Frase[i] == maiusculo[j])
+                    {
+                        qtdMaiusculo++;
+                    }
+                }
+            }
+
+            Console.WriteLine("Quantidades de letras maíusculas: " + qtdMaiusculo);
+
+        }
+        public void ValidaMinuscula()
+        {
+            string minusculo = "abcdefghijklmnopqrstuvwxyz";
+            int qtdMinusculo = 0;
+
+            for (int i = 0; i < Frase.Length; i++)
+            {
+                for (int j = 0; j < minusculo.Length; j++)
+                {
+                    if (Frase[i] == minusculo[j])
+                    {
+                        qtdMinusculo++;
+                    }
+                }
+            }
+
+            Console.WriteLine("Quantidades de letras minúsculas: " + qtdMinusculo);
+        }
+        public void ValidaConsoante()
+        {
+            string consoantes = "BCDFGHJKLMNPQRSTVWXYZbcdfghjklmnpqrstvwxy";
+            int numeroConsoantes = 0;
+
+            for (int i = 0; i < Frase.Length; i++)
+            {
+                for (int j = 0; j < consoantes.Length; j++)
+                {
+                    if (Frase[i] == consoantes[j])
+                    {
+                        numeroConsoantes++;
+                    }
+                }
+            }
+
+            Console.WriteLine("Quantidade de consoantes: " + numeroConsoantes);
+
+        }
+        public void ValidaVogal()
+        {
+
+            string vogais = "AEIOUaeiou";
+            int numeroVogais = 0;
+
+
+            for (int i = 0; i < Frase.Length; i++)
+            {
+                for (int j = 0; j < vogais.Length; j++)
+                {
+                    if (Frase[i] == vogais[j])
+                    {
+                        numeroVogais++;
+                    }
+                }
+            }
+
+            Console.WriteLine("Quantidades de vogais: " + numeroVogais);
+        }
+
+        public void ValidaQtdCaracteres()
+        {
+            Console.WriteLine("Quantidade de caracteres na frase: " + Frase.Length);
+        }
+        public void ValidaQtdPalavra()
+        {
+            string espaco = " ";
+            int qtdEspaco = 0;
+
+            for (int i = 0; i < Frase.Length; i++)
+            {
+                for (int j = 0; j < espaco.Length; j++)
+                {
+                    if (Frase[i] == espaco[j])
+                    {
+                        qtdEspaco++;
+                    }
+                }
+            }
+            Console.WriteLine($"Quantidade de palavras: {qtdEspaco + 1}");
+        }
+
+        //public void ValidaQtdPalavraOpcao2()
+        //{
+
+        //    var contagem = Frase.Split(' ');
+
+        //    Console.WriteLine(contagem.Length);
+        //}
+    }
+}
